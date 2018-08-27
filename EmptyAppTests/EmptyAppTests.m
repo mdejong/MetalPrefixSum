e0d81b0286bfe4377f15b2373f788f7ad7bffe14
Mo DeJong <mo@modejong.com>
2018-08-27 00:57:56 -0700

    finish up first reduce test case

--- EmptyAppTests/EmptyAppTests.m
+++ EmptyAppTests/EmptyAppTests.m
@@ -10,6 +10,7 @@
 
 #import "MetalRenderContext.h"
 #import "MetalPrefixSumRenderContext.h"
+#import "MetalPrefixSumRenderFrame.h"
 
 @interface EmptyAppTests : XCTestCase
 
@@ -27,6 +28,87 @@
     [super tearDown];
 }
 
+// Query a texture that contains byte values and return in
+// a buffer of uint8_t typed values.
+
++ (NSData*) getTextureBytes:(id<MTLTexture>)texture
+{
+  int width = (int) texture.width;
+  int height = (int) texture.height;
+  
+  NSMutableData *mFramebuffer = [NSMutableData dataWithLength:width*height*sizeof(uint8_t)];
+  
+  [texture getBytes:(void*)mFramebuffer.mutableBytes
+        bytesPerRow:width*sizeof(uint8_t)
+      bytesPerImage:width*height*sizeof(uint8_t)
+         fromRegion:MTLRegionMake2D(0, 0, width, height)
+        mipmapLevel:0
+              slice:0];
+  
+  return [NSData dataWithData:mFramebuffer];
+}
+
+// Dump texture that contains simple grayscale pixel values
+
+- (void) dump8BitTexture:(id<MTLTexture>)outTexture
+                   label:(NSString*)label
+{
+  // Copy texture data into debug framebuffer, note that this include 2x scale
+  
+  int width = (int) outTexture.width;
+  int height = (int) outTexture.height;
+  
+  NSData *bytesData = [self.class getTextureBytes:outTexture];
+  uint8_t *bytesPtr = (uint8_t*) bytesData.bytes;
+  
+  // Dump output words as bytes
+  
+  if ((1)) {
+    fprintf(stdout, "%s as bytes\n", [label UTF8String]);
+    
+    for ( int row = 0; row < height; row++ ) {
+      for ( int col = 0; col < width; col++ ) {
+        int offset = (row * width) + col;
+        uint8_t v = bytesPtr[offset];
+        fprintf(stdout, "%3d ", v);
+      }
+      fprintf(stdout, "\n");
+    }
+    
+    fprintf(stdout, "done\n");
+  }
+}
+
+// Return contents of 8 bit texture as NSArray number values
+
+- (NSArray*) arrayFrom8BitTexture:(id<MTLTexture>)outTexture
+{
+  // Copy texture data into debug framebuffer, note that this include 2x scale
+  
+  int width = (int) outTexture.width;
+  int height = (int) outTexture.height;
+  
+  NSData *bytesData = [self.class getTextureBytes:outTexture];
+  uint8_t *bytesPtr = (uint8_t*) bytesData.bytes;
+  
+  // Dump output words as bytes
+  
+  NSMutableArray *mArr = [NSMutableArray array];
+  
+  {
+    for ( int row = 0; row < height; row++ ) {
+      for ( int col = 0; col < width; col++ ) {
+        int offset = (row * width) + col;
+        uint8_t v = bytesPtr[offset];
+        
+        [mArr addObject:@(v)];
+      }
+    }
+  }
+  
+  return mArr;
+}
+
 - (void)testMetalExample {
   // This is an example of a functional test case.
   // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -41,7 +123,65 @@
   
   [mpsrc setupRenderPipelines:mrc];
   
-  XCTAssert(true);
+  MetalPrefixSumRenderFrame *mpsrf = [[MetalPrefixSumRenderFrame alloc] init];
+  
+  CGSize renderSize = CGSizeMake(4, 4);
+  
+  [mpsrc setupRenderTextures:mrc renderSize:renderSize renderFrame:mpsrf];
+  
+  // Get a metal command buffer
+  
+  id <MTLCommandBuffer> commandBuffer = [mrc.commandQueue commandBuffer];
+  
+#if defined(DEBUG)
+  assert(commandBuffer);
+#endif // DEBUG
+  
+  commandBuffer.label = @"XCTestRenderCommandBuffer";
+  
+  // Prefix sum setup and render steps
+  
+  [mpsrc renderPrefixSumReduce:mrc commandBuffer:commandBuffer renderFrame:mpsrf];
+  
+  // Wait for commands to be rendered
+  [commandBuffer commit];
+  [commandBuffer waitUntilCompleted];
+
+  // Dump output of render process
+  
+  BOOL dump = TRUE;
+  
+  id<MTLTexture> inputTexture = (id<MTLTexture>) mpsrf.inputBlockOrderTexture;
+  
+  if (dump) {
+  [self dump8BitTexture:inputTexture label:@"inputTextureD1"];
+  }
+  
+  id<MTLTexture> outputTexture = (id<MTLTexture>) mpsrf.reduceTextures[0];
+  
+  if (dump) {
+  [self dump8BitTexture:outputTexture label:@"outputTextureD1"];
+  }
+
+  NSArray *inputArr = [self arrayFrom8BitTexture:inputTexture];
+  NSArray *renderedArr = [self arrayFrom8BitTexture:outputTexture];
+
+  NSArray *epectedInputArr = @[
+                                  @0, @1, @2, @3,
+                                  @4, @5, @6, @7,
+                                  @8, @9, @10, @11,
+                                  @12, @13, @14, @15
+                                  ];
+  
+  NSArray *epectedRenderedArr = @[
+                                  @1, @5,
+                                  @9, @13,
+                                  @17, @21,
+                                  @25, @29
+                                  ];
+  
+  XCTAssert([inputArr isEqualToArray:epectedInputArr]);
+  XCTAssert([renderedArr isEqualToArray:epectedRenderedArr]);
 }
 
 //- (void)testPerformanceExample {
